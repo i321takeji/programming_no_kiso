66dd39c5fa7857ed87e95955c5896ace58f7783c
i321takeji <i.321.takeji@gmail.com>
2025-10-27 00:08:19 +0900

    Add ch03, ch04

---
+++ ch04/ch04.ml
@@ -0,0 +1,11 @@
+(* exer 4.1 *)
+let baito_kyuyo y h = (y * 100 + 850) * h ;;
+
+(* exer 4.2 *)
+let jikoshokai name = "Hello! " ^ name ;;
+
+(* exer 4.3 *)
+let hyojun_taiju h = h ** 2.0 *. 22.0
+
+(* exer 4.4 *)
+let bmi h w = w /. (h ** 2.0) ;;
