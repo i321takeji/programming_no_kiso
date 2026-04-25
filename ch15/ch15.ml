20dd23b07f2dca0986a366e44a1e5b7658bf8483
i321takeji <i.321.takeji@gmail.com>
2026-04-25 23:39:56 +0900

    Add up to ex16.1

--- ch15/ch15.ml
+++ ch15/ch15.ml
@@ -47,3 +47,64 @@ let rec e n =
     d
   else
     d +. e (n + 1)
+
+(* ex15.2 *)
+(* 目的：ふたつの自然数 m と n の最大公約数を求める *)
+(* gcd : int -> int -> int *)
+let rec gcd m n =
+  if n = 0 then
+    m
+  else
+    gcd n (m mod n)
+
+(* テストケース *)
+let test1_gcd = gcd 0 5 = 5
+
+let test2_gcd = gcd 5 0 = 5
+
+let test3_gcd = gcd 12 18 = 6
+
+let test4_gcd = gcd 13 17 = 1
+
+let test5_gcd = gcd 24 6 = 6
+
+(* ex15.3 *)
+(* 目的：2 <= n の自然数のリストを受け取ったら，2 <= n 以下の素数のリストを返す関数 *)
+(* sieve : int list -> int list *)
+let rec sieve lst =
+  match lst with
+  | [] -> []
+  | first :: rest ->
+      first :: sieve (List.filter (fun n -> n mod first <> 0) rest)
+
+(* 目的：2 から n までの自然数のリスト *)
+let from_2_to_n n =
+  let rec f i =
+    if i <= n then
+      i :: f (i + 1)
+    else
+      []
+  in
+  f 2
+
+(* 目的：自然数を受け取ったら，それ以下の素数のリストを返す関数 *)
+let prime n = sieve (from_2_to_n n)
+
+let test1_sieve = sieve [] = []
+
+let test2_sieve = sieve [2] = [2]
+
+let test3_sieve = sieve [2; 3; 4; 5; 6; 7; 8; 9; 10] = [2; 3; 5; 7]
+
+let test4_sieve =
+  sieve [2; 3; 4; 5; 6; 7; 8; 9; 10; 11; 12; 13] = [2; 3; 5; 7; 11; 13]
+
+let test1_prime = prime 0 = []
+
+let test2_prime = prime 1 = []
+
+let test3_prime = prime 2 = [2]
+
+let test4_prime = prime 10 = [2; 3; 5; 7]
+
+let test5_prime = prime 13 = [2; 3; 5; 7; 11; 13]
---
+++ ch16/ch16.ml
@@ -0,0 +1,40 @@
+(* 距離と距離の合計を持っている型 *)
+type distance_t = { kyori : float (* 距離 *); total : float (* 距離の合計 *) }
+
+(* 目的：先頭からリスト中の各点までの距離の合計を計算する *)
+(* total_distance : distance_t list -> distance_t list *)
+let total_distance lst =
+  (* 目的：先頭からリスト中の各点までの距離の合計を計算する *)
+  (* ここで total0 はこれまでの距離の合計 *)
+  (* hojo : distance_t list -> float -> distance_t list *)
+  let rec hojo lst total0 =
+    match lst with
+    | [] -> []
+    | { kyori = k; total = t } :: rest ->
+        { kyori = k; total = total0 +. k } :: hojo rest (total0 +. k)
+  in
+  hojo lst 0.0
+
+(* ex16.1 *)
+(* 目的：整数のリストを受け取ったら，それまでの数の合計からなるリストを返す関数 *)
+(* sum_list : int list -> int list *)
+let sum_list lst =
+  let rec hojo lst total0 =
+    match lst with
+    | [] -> []
+    | first :: rest ->
+        let acc = total0 + first in
+        acc :: hojo rest acc
+  in
+  hojo lst 0
+
+(* テストケース *)
+let test1_sum_list = sum_list [] = []
+
+let test2_sum_list = sum_list [3] = [3]
+
+let test3_sum_list = sum_list [3; 2] = [3; 5]
+
+let test4_sum_list = sum_list [3; 2; 1] = [3; 5; 6]
+
+let test5_sum_list = sum_list [3; 2; 1; 4] = [3; 5; 6; 10]
