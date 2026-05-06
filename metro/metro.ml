8c0cf73b8f28cdb50d2b34b520d6abb8ab0d9fe2
i321takeji <i.321.takeji@gmail.com>
2026-05-07 00:04:38 +0900

    Add testcase

--- metro/metro.ml
+++ metro/metro.ml
@@ -1542,3 +1542,27 @@ let test3_saitan_wo_bunri =
     ; { namae = "新宿"; saitan_kyori = 0.0; temae_list = ["新宿"] } ]
   = ( { namae = "新宿"; saitan_kyori = 0.0; temae_list = ["新宿"] }
     , [{ namae = "茗荷谷"; saitan_kyori = 3.0; temae_list = [] }] )
+
+let eki1 = { namae = "池袋"; saitan_kyori = 0.0; temae_list = ["池袋"] }
+
+let eki2 = { namae = "新大塚"; saitan_kyori = 1.8; temae_list = ["新大塚"; "池袋"] }
+
+let eki3 =
+  { namae = "茗荷谷"; saitan_kyori = 3.0; temae_list = ["茗荷谷"; "新大塚"; "池袋"] }
+
+let eki4 = { namae = "後楽園"; saitan_kyori = infinity; temae_list = [] }
+
+let test_saitan_wo_bunri1 =
+  saitan_wo_bunri []
+  = ({ namae = "dummy"; saitan_kyori = infinity; temae_list = [] }, [])
+
+let test_saitan_wo_bunri2 = saitan_wo_bunri [eki1] = (eki1, [])
+
+let test_saitan_wo_bunri3 =
+  saitan_wo_bunri [eki1; eki2; eki3] = (eki1, [eki2; eki3])
+
+let test_saitan_wo_bunri4 =
+  saitan_wo_bunri [eki3; eki1; eki2] = (eki1, [eki2; eki3])
+
+let test_saitan_wo_bunri5 =
+  saitan_wo_bunri [eki4; eki3; eki2] = (eki2, [eki3; eki4])
