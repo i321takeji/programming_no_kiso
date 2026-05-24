fb5b22cf232b6e76a837b45f993f4a2e1eaddd68
i321takeji <i.321.takeji@gmail.com>
2026-05-25 00:00:33 +0900

    Add up to sec17.1

--- ch17/ch17.ml
+++ ch17/ch17.ml
@@ -1,3 +1,31 @@
+(* sec17.1 *)
+
+(* 赤組か白組かを表す型 *)
+type team_t = Red | White
+
+(* 目的：受け取ったチーム名を文字列で返す *)
+(* team_string : team_t -> string *)
+let team_string team =
+  match team with
+  | Red -> "赤組"
+  | White -> "白組"
+
+(* 年号を表す型 *)
+type nengou_t =
+  | Meiji of int (* 明治 *)
+  | Taisho of int (* 大正 *)
+  | Showa of int (* 昭和 *)
+  | Heisei of int (* 平成 *)
+
+(* 目的：年号を受け取ったら対応する西暦年を返す *)
+(* to_seireki : nengou_t -> int *)
+let to_seireki nengou =
+  match nengou with
+  | Meiji n -> n + 1867
+  | Taisho n -> n + 1911
+  | Showa n -> n + 1925
+  | Heisei n -> n + 1988
+
 (* sec17.2 *)
 
 (* 木を表す型 *)
